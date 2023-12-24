421f6da81387dcb8ae9783e99779ba71d7eb954a
Yugkumar Ashishkumar Mistry <139417149+Yug-Mistry@users.noreply.github.com>
2023-12-24 20:43:19 +0530

    Delete lab12 directory

--- lab12/lab12_1/app3/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app3.models import Job, Book, Customer
-
-# Register your models here.
-
-admin.site.register(Job)
-admin.site.register(Book)
-admin.site.register(Customer)
