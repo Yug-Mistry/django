fb6cb494a117b5919c0259302d918962ff561aa4
Yugkumar Ashishkumar Mistry <139417149+Yug-Mistry@users.noreply.github.com>
2023-12-24 20:46:04 +0530

    Delete lab12_1 directory

--- lab12_1/app2/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from app2.models import Employee
-
-# Register your models here.
-admin.site.register(Employee)
--- lab12_1/app2/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from app2 import views as app2_views
-
-urlpatterns = [
-    path('showEmployees/',app2_views.show_employees, name='show_employees'),
-]
